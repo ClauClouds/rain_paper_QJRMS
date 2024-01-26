a76573e7390b53553e6b7dc83cf713458fc5d79f
Atmo Acqui <37664450+ClauClouds@users.noreply.github.com>
2024-01-26 17:35:17 +0100

    added reading for mesoscale pattern file
    added reading function for mesoscale pattern classification from the file of Isabel McCoy

--- readers/cloudtypes.py
+++ readers/cloudtypes.py
@@ -24,3 +24,31 @@ def read_cloudtypes():
     )
 
     return ds
+
+
+def read_merian_classification():
+    """
+    function to read merian mesoscale classification from Isabel Mc Coy
+    returns:
+    ds: xarray dataset with classification data
+    
+    """
+    
+    ds = xr.open_dataset('/data/obs/campaigns/eurec4a/msm/EUREC4A_ATOMIC_C3ONTEXTClassifications_CollocatedTo_MS-Merian_ManualInstant_IR_FreqLim_0.5.nc', decode_times=False)
+    
+    ds_new = preprocess_class(ds)
+    
+    
+    return ds_new
+
+def preprocess_class(ds):
+    """
+    preprocess time stamps converting them from fractional day of the year to datetime
+    Args:
+        ds (xarray dataset): dataset with mesoscale classification data and additional datetime coordinate
+    """
+    
+    ds['datetime'] = pd.to_datetime(ds.day_frac.values, unit='D', origin=pd.Timestamp('01-01-2020'))
+    
+    return(ds)
+
