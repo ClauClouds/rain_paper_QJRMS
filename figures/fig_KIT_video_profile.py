dc1306d2a98cefe24275353d34ef8ada0081e0d8
Clau Clouds <37664450+ClauClouds@users.noreply.github.com>
2025-01-08 11:10:05 +0100

    scatter plot and profile rs vs arthus

--- figures/fig_KIT_video_profile.py
+++ figures/fig_KIT_video_profile.py
@@ -12,7 +12,7 @@ import pandas as pd
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
+from figures.mpl_style import cmap_gif, cmap_gif2
 import numpy as np
 import xarray as xr
 #from dotenv import load_dotenv
@@ -24,47 +24,111 @@ from metpy.units import units
 import pdb
 from dask.diagnostics import ProgressBar
 ProgressBar().register()
+import os 
+import pdb
+import imageio.v3 as iio
+
+from PIL import Image
+import glob
 
 # define path for plots
 from readers.radar import read_radar_single_file
+
+
 def main():
 
-    path_paper_plots = "/net/ostro/plots_rain_paper/ZE_gif/"
-
+    # set directory where to store plots and gif
+    directory = path_paper_plots+'/Ze_gif/'
+    
     # read radar reflectivity
     ds = read_radar_single_file('2020', '02', '19')    
-    # select a rainy day
-    print(ds)
+    # select some hours for plotting
+    ds_sel = ds.sel(time=slice('2020-02-19T00:20:00', '2020-02-19T00:40:00'))
+    print(ds_sel)
+
+    # plot radar reflectivity profiles
+    plot_profiles(ds_sel, directory)
+
+    # list filenames 
+    create_gif(directory)
+
+
+def create_gif(directory):
+    """
+    # Create a list to store the images
+    """
+    frames = []
+    imgs = sorted(glob.glob(directory+"/*.png"))
+
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+
+    # Save into a GIF file that loops forever
+    frames[0].save(path_paper_plots+'/png_to_gif.gif', format='GIF',
+                append_images=frames[1:],
+                save_all=True,
+                duration=300, loop=0)
+    print(f"GIF created at {directory}")
+
+    return(None)
+
+
+    
+def plot_profiles(ds_sel, directory):
+    """
+    script to produce plots of radar reflectivity profiles
+
+    Parameters
+    ----------
+    ds_sel : xarray dataset
+        dataset with radar reflectivity and mean doppler velocity
+    directory : str
+        directory where to store plots and gif
+    """
+    # set all fonts size
+    plt.rcParams.update({'font.size': 20})
 
     # setting cmap edges
-    cmap_min = -4
-    cmap_max = 4
+    cmap_min = -2
+    cmap_max = 2
 
     # loop on time 
-    for i_t, time_val in enumerate(ds.time.values):
+    for i_t, time_val in enumerate(ds_sel.time.values):
         print(time_val)
 
         # convert time_val to string yymmdd_hhmmss
         time_val_str = pd.to_datetime(time_val).strftime("%y%m%d_%H%M%S")
-
         fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+        ax.plot(ds_sel.sel(time=time_val).radar_reflectivity, 
+                ds_sel.height, 
+                label='radar reflectivity', 
+                color='black')
         line = plot_colored_line(ax, 
-                      ds.sel(time=time_val).radar_reflectivity, 
-                      ds.height, 
-                      ds.sel(time=time_val).height,
-                      cmap_min,
-                      cmap_max)
-        .plot(ax=ax, cmap=CMAP, vmin=-20, vmax=20)
-        ax.set_title(time_val)
-        plt.savefig(f"{path_paper_plots}/{time_val_str}_radar_reflectivity.png")
+                          ds_sel.sel(time=time_val).radar_reflectivity, 
+                          ds_sel.height, 
+                          ds_sel.sel(time=time_val).mean_doppler_velocity, 
+                          cmap_gif, 
+                          cmap_min, 
+                          cmap_max)
+        
+
+        # add colorbar
+        cbar = plt.colorbar(line, ax=ax)
+        #add cbar Label
+        cbar.set_label('Mean Doppler Velocity (m/s)')
+
+        ax.set_xlim(-45, 20)
+        ax.set_ylim(0, 4000)
+        ax.set_ylabel('Height (m)')
+        ax.set_xlabel('Radar Reflectivity (dBZ)')
+        plt.savefig(f"{directory}/{time_val_str}_radar_reflectivity.png", 
+                    transparent=True)
         plt.close()
+    return(None)
 
-    
-    # create gif
-    os.system(f"convert -delay 100 -loop 0 {path_paper_plots}/*_radar_reflectivity.png {path_paper_plots}/radar_reflectivity.gif")
-    
 
-def plot_colored_line(ax, x, y, z, cmap_name, label, cmap_min, cmap_max):
+def plot_colored_line(ax, x, y, z, cmap_name, cmap_min, cmap_max):
     
     from matplotlib.collections import LineCollection
     from matplotlib.colors import ListedColormap, BoundaryNorm
@@ -80,30 +144,13 @@ def plot_colored_line(ax, x, y, z, cmap_name, label, cmap_min, cmap_max):
     # Create a continuous norm to map from data points to colors
     norm = plt.Normalize(cmap_min, cmap_max)
 
+    lc = LineCollection(segments, cmap=cmap_name, norm=norm)
+    # Set the values used for colormapping
+    lc.set_array(z)
+    line = ax.add_collection(lc)
+    lc.set_linewidth(4)
 
-    # set line style based on class
-    if label == 'congestus non prec':
-        lc = LineCollection(segments, cmap=cmap_name, norm=norm)
-        # Set the values used for colormapping
-        lc.set_array(z)
-        line = ax.add_collection(lc)
-        line.set_linestyle('--')
-        lc.set_linewidth(4)
-    elif label == 'congestus prec':
-        lc = LineCollection(segments, cmap=cmap_name, norm=norm)
-        # Set the values used for colormapping
-        lc.set_array(z)
-        line = ax.add_collection(lc)
-        line.set_linestyle('solid')
-        lc.set_linewidth(6)
-    else:
-        lc = LineCollection(segments, cmap=cmap_name, norm=norm)
-        # Set the values used for colormapping
-        lc.set_array(z)
-        line = ax.add_collection(lc)
-        line.set_linestyle('--')
-        lc.set_linewidth(2)
-    ax.legend(frameon=False)
+    
 
     return(line)
 
